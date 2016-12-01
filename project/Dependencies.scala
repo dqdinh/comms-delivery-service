cef3a62072beedc7a1461c23588ff7582ea66b5f
Chris Birchall <chris.birchall@ovoenergy.com>
2016-12-01 11:52:37 +0000

    Bump comms-kafka-*

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,8 @@ import sbt._
 
 object Dependencies {
 
+  val kafkaMessagesVersion = "0.0.12"
+
   def all() = Seq(
     "com.typesafe.akka"           %% "akka-stream-kafka"              % "0.12",
     "com.typesafe.akka"           %% "akka-slf4j"                     % "2.3.14",
@@ -13,8 +15,8 @@ object Dependencies {
     "me.moocar"                    % "logback-gelf"                   % "0.2",
     "net.cakesolutions"           %% "scala-kafka-client"             % "0.10.0.0",
     "io.logz.logback"              % "logzio-logback-appender"        % "1.0.11",
-    "com.ovoenergy"               %% "comms-kafka-messages"           % "0.0.11",
-    "com.ovoenergy"               %% "comms-kafka-serialisation"      % "0.0.11",
+    "com.ovoenergy"               %% "comms-kafka-messages"           % kafkaMessagesVersion,
+    "com.ovoenergy"               %% "comms-kafka-serialisation"      % kafkaMessagesVersion,
     "org.typelevel"               %% "cats-core"                      % "0.8.0",
     "com.squareup.okhttp3"         % "okhttp"                         % "3.4.2",
     "org.mockito"                  % "mockito-all"                    % "1.10.19"     %   Test,
