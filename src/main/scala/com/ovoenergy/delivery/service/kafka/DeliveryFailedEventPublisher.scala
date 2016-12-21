5e3994775a0a87b4ce5b19d0835566bf899f5dfa
Chris Birchall <chris.birchall@ovoenergy.com>
2016-12-21 15:09:51 +0000

    Use customerId as Kafka partition key

--- src/main/scala/com/ovoenergy/delivery/service/kafka/DeliveryFailedEventPublisher.scala
+++ src/main/scala/com/ovoenergy/delivery/service/kafka/DeliveryFailedEventPublisher.scala
@@ -5,14 +5,22 @@ import cakesolutions.kafka.KafkaProducer.Conf
 import com.ovoenergy.comms.model.Failed
 import com.ovoenergy.comms.serialisation.Serialisation._
 import com.ovoenergy.delivery.service.kafka.domain.KafkaConfig
-import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.{ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.StringSerializer
+import scala.concurrent.Future
 
 object DeliveryFailedEventPublisher {
 
-  def apply(kafkaConfig: KafkaConfig)(failed: Failed) = {
-    val deliveryFailedProducer = KafkaProducer(Conf(new StringSerializer, avroSerializer[Failed], kafkaConfig.hosts))
-    deliveryFailedProducer.send(new ProducerRecord[String, Failed](kafkaConfig.commFailedTopic, failed))
+  def apply(kafkaConfig: KafkaConfig): Failed => Future[RecordMetadata] = {
+    val producer = KafkaProducer(Conf(new StringSerializer, avroSerializer[Failed], kafkaConfig.hosts))
+
+    (failed: Failed) => {
+      producer.send(new ProducerRecord[String, Failed](
+        kafkaConfig.commFailedTopic, 
+        failed.metadata.customerId,
+        failed
+      ))
+    }
   }
 
 }
--- src/main/scala/com/ovoenergy/delivery/service/kafka/DeliveryProgressedEventPublisher.scala
+++ src/main/scala/com/ovoenergy/delivery/service/kafka/DeliveryProgressedEventPublisher.scala
@@ -3,15 +3,23 @@ package com.ovoenergy.delivery.service.kafka
 import cakesolutions.kafka.KafkaProducer
 import cakesolutions.kafka.KafkaProducer.Conf
 import com.ovoenergy.comms.model.EmailProgressed
-import com.ovoenergy.delivery.service.kafka.domain.KafkaConfig
-import org.apache.kafka.clients.producer.ProducerRecord
 import com.ovoenergy.comms.serialisation.Serialisation._
+import com.ovoenergy.delivery.service.kafka.domain.KafkaConfig
+import org.apache.kafka.clients.producer.{ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.StringSerializer
+import scala.concurrent.Future
 
 object DeliveryProgressedEventPublisher {
 
-  def apply(kafkaConfig: KafkaConfig)(progressed: EmailProgressed) = {
-    val deliveryProgressedProducer  = KafkaProducer(Conf(new StringSerializer, avroSerializer[EmailProgressed], kafkaConfig.hosts))
-    deliveryProgressedProducer.send(new ProducerRecord[String, EmailProgressed](kafkaConfig.emailProgressedTopic, progressed))
+  def apply(kafkaConfig: KafkaConfig): EmailProgressed => Future[RecordMetadata] = {
+    val producer  = KafkaProducer(Conf(new StringSerializer, avroSerializer[EmailProgressed], kafkaConfig.hosts))
+
+    (progressed: EmailProgressed) => {
+      producer.send(new ProducerRecord[String, EmailProgressed](
+        kafkaConfig.emailProgressedTopic, 
+        progressed.metadata.customerId, 
+        progressed
+      ))
+    }
   }
 }
