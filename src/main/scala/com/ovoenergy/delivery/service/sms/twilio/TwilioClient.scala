7d5bb0bcca1f04e4580dafa991e855942fa400fc
Filippo De Luca <filippo.deluca@ovoenergy.com>
2018-10-18 13:28:50 +0100

    Change the twilio sids to snake case in credstash

--- src/main/scala/com/ovoenergy/delivery/service/sms/twilio/TwilioClient.scala
+++ src/main/scala/com/ovoenergy/delivery/service/sms/twilio/TwilioClient.scala
@@ -107,14 +107,14 @@ object TwilioClient extends LoggingWithMDC {
   }
 
   private[twilio] def serviceSid(sids: TwilioServiceSids, brand: Brand) = brand match {
-    case Ovo   => sids.ovo
-    case Boost => sids.boost
-    case Corgi => sids.corgi
-    case Lumo  => sids.lumo
-    case Vnet  => sids.vnet
-    case EnergySW => sids.energySw
-    case Fairerpower => sids.fairerpower
+    case Ovo                => sids.ovo
+    case Boost              => sids.boost
+    case Corgi              => sids.corgi
+    case Lumo               => sids.lumo
+    case Vnet               => sids.vnet
+    case EnergySW           => sids.energySw
+    case Fairerpower        => sids.fairerpower
     case PeterboroughEnergy => sids.peterboroughEnergy
-    case SouthendEnergy => sids.southendEnergy
+    case SouthendEnergy     => sids.southendEnergy
   }
 }
